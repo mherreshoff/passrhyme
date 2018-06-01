fd8255c468fedfe1751fcabdd3a0db1c74e8d76f
Marcello Herreshoff <marcello.herreshoff@gmail.com>
2018-05-31 19:30:10 -0700

    Some docs.

--- src/Choice.hs
+++ src/Choice.hs
@@ -12,29 +12,40 @@ import System.Random
 
 ----- Choice Data-Structure:
 data Choice a = Choice Integer (Integer -> a)
--- A 'Choice a' object represents a uniform distribution over a set of 'a's.
+-- A 'Choice a' object represents an indexed collection of 'a's.
+--
 -- It knows how many there are and how to pick one given an index.
--- It is more efficient than a list because the possibilities need not all be
--- layed out in memory.
+--
+-- We use this data-structure instead of, say, a list because, by
+-- using mechanisms like `choiceProduct`, we can create Choice
+-- objects which represent lists far larger than could fit in memory.
 
 
 ----- Accessors:
 countChoices :: Choice a -> Integer
 countChoices (Choice n _) = n
 
-choiceBits :: Choice a -> Double
-choiceBits = logBase 2.0 . fromIntegral . countChoices
-
 choose :: Choice a -> Integer -> a
 choose (Choice _ c) idx = c idx
+
+
+------ Probabilistic Interpretation:
+-- We can interpret a (non-empty) choice object as a uniform
+-- distribution over its elements.
+
+choiceBits :: Choice a -> Double
+choiceBits = logBase 2.0 . fromIntegral . countChoices
+-- Measures (in bits) the entropy of a choice (interpreted as a uniform distribution.)
 
 chooseRandomly :: (Show g, RandomGen g) => Choice a -> g -> (a, g)
 chooseRandomly c g = (choose c idx, g') where
   (idx, g') = randomR (0, countChoices c - 1) g where
+-- Samples from a choice (interpreted as a uniform distribution.)
 
 
 ----- Instances:
 instance Monoid (Choice a) where
+  -- The Monoid instance unions choices together.  See `choiceUnion`.
   mempty = Choice 0 (\idx -> error "Attempted to choose from empty choice.")
   mappend a b = mconcat [a, b]
   mconcat = choiceUnion
